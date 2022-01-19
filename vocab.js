b76ca565566964de095500043807245ab1e56c66
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-01-19 21:47:00 +0000

    updated with vocab.js

---
+++ vocab.js
@@ -0,0 +1,14 @@
+
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var vocab = ["a", "about", "all", "also", "and", "as", "at", "be", "because", "but", "by", "can", "come", "could", "day", "do", "even", "find", "first", "for", "from", "get", "give", "go", "have", "he", "her", "here", "him", "his", "how", "i", "if", "in", "into", "it", "its", "just", "know", "like", "look", "make", "man", "many", "me", "more", "my", "new", "no", "not", "now", "of", "on", "one", "only", "or", "other", "our", "out", "people", "say", "see", "she", "some", "take", "tell", "than", "that", "the", "their", "them", "then", "there", "these", "they", "thing", "think", "this", "those", "time", "to", "two", "up", "us", "every", "want", "way", "we", "well", "what", "when", "which", "who", "will", "with", "would", "year", "you", "your"]
+
+function checkVocab(newWord)
+vocab.indexOf(string) !== -1 {
+    alert("This already exsists in my vocab")
+}
+
+function newVocab(newWord) {
+    vocab.push(newVocab);
+}
