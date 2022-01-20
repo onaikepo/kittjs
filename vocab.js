40dc3bf4356f729cee4ed75cdbd832a596df4799
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-01-20 21:40:24 +0000

    updated vocab.js with new push function

--- vocab.js
+++ vocab.js
@@ -4,11 +4,17 @@ var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 
 var vocab = ["a", "about", "all", "also", "and", "as", "at", "be", "because", "but", "by", "can", "come", "could", "day", "do", "even", "find", "first", "for", "from", "get", "give", "go", "have", "he", "her", "here", "him", "his", "how", "i", "if", "in", "into", "it", "its", "just", "know", "like", "look", "make", "man", "many", "me", "more", "my", "new", "no", "not", "now", "of", "on", "one", "only", "or", "other", "our", "out", "people", "say", "see", "she", "some", "take", "tell", "than", "that", "the", "their", "them", "then", "there", "these", "they", "thing", "think", "this", "those", "time", "to", "two", "up", "us", "every", "want", "way", "we", "well", "what", "when", "which", "who", "will", "with", "would", "year", "you", "your"]
 
-function checkVocab(newWord)
+/*function checkVocab(newWord)
 vocab.indexOf(string) !== -1 {
     alert("This already exsists in my vocab")
-}
+}*/
 
 function newVocab(newWord) {
-    vocab.push(newVocab);
+    if (newWord == ! "a") {
+        vocab.push(newWord);
+        console.log("I learnt a new word" + " " + newWord)
+    } else {
+        console.log("I already know the word" + newWord)
+    }
 }
+
